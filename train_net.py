0b5617b03b2a44543a24c81150f462f871e89932
Yun Jia <yunjia@me.com>
2022-08-21 00:24:20 -0700

    Add training for customset balloon.

--- train_net.py
+++ train_net.py
@@ -6,6 +6,7 @@ from torch.nn.parallel import DistributedDataParallel
 import time
 import datetime
 import json
+import balloon
 
 from fvcore.common.timer import Timer
 import detectron2.utils.comm as comm
